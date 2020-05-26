e0ce350356c92e6854218cc013b7f72f36dbb4cd
Marcos Cardenas-Zelaya <marcos.cardenas-zelaya@mail.mcgill.ca>
2020-05-26 00:24:35 -0400

    added script to speed up marking in crowdmark

--- keypad.py
+++ keypad.py
@@ -8,9 +8,9 @@ for event in keyboard.read_loop():
   if event.type == ecodes.EV_KEY:
       key = categorize(event)
       if key.keystate == key.key_down:
-          if key.keycode == 'KEY_KPSLASH': #go to tab to the left
+          if key.keycode == 'KEY_KPSLASH': #go to previous page
                os.system('xdotool key ctrl+Prior')
-          if key.keycode == 'KEY_KPASTERISK': #go to tab to te right
+          if key.keycode == 'KEY_KPASTERISK': #go to tab to next page
                os.system('xdotool key ctrl+Next')
           if key.keycode == 'KEY_KP0': #to window tab to the left
                os.system('xdotool key Alt_R+Left')
@@ -20,3 +20,5 @@ for event in keyboard.read_loop():
                os.system('xdotool click 1')
           if key.keycode == 'KEY_KPMINUS': #close current tab in chrome
                os.system('xdotool key ctrl+F4')
+          if key.keycode == 'KEY_KPDOT': #open link in tab in chrome
+               os.system('/home/cos/Documents/coding/scripts-and-bits/crowdmarker')
